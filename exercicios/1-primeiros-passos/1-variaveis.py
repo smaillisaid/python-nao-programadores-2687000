3c9abee67c9da1db75c6298e683bd9fb22f364c9
smaillisaid <78044160+smaillisaid@users.noreply.github.com>
2026-01-07 04:06:26 +0000

    Resposta exercício

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,10 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Said'
+sobrenome = 'Calil'
 # Declare variáveis de cada tipo numérico
-
-
+valor = 33
+custo = 22977.99
 # Declare uma variável do tipo booleano
+vitoria = True
