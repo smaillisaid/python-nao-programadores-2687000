9802f7d314deac7ca79b0001e831cdfa5c5da8b9
smaillisaid <78044160+smaillisaid@users.noreply.github.com>
2026-01-07 04:44:50 +0000

    Resposta dos exercícios

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,13 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
-
+nascimento_idade = data_nascimento + str(idade_numerica)
+print(nascimento_idade)
 # Realize uma operação entre dados do tipo inteiro e float
+idade_altura = idade_numerica + altura
+print(idade_altura)
